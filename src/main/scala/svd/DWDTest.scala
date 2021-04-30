1c1db39c8862ac150dcb8e8d09dd8953872a5785
sprk <cebaraldi@gmail.com>
2021-04-30 21:42:27 +0200

    SVD using 3 weather stations, matrix has dimension 366 x 3. Maybe matrix has to be transposed...maybe not?

--- src/main/scala/svd/DWDTest.scala
+++ src/main/scala/svd/DWDTest.scala
@@ -1,8 +1,5 @@
 package svd
 
-import collection.JavaConverters._
-//import scala.collection.breakOut
-
 import org.apache.spark.mllib.linalg
 import org.apache.spark.mllib.linalg.distributed.RowMatrix
 import org.apache.spark.mllib.linalg.{Matrix, SingularValueDecomposition, Vector, Vectors}
@@ -11,6 +8,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
 import java.time.LocalDate
+import scala.collection.JavaConverters._
 
 object DWDTest extends App {
 
@@ -21,10 +19,8 @@ object DWDTest extends App {
   spark.sparkContext.setLogLevel("WARN")
 
   val currentDate = LocalDate.now()
-  val startDate = LocalDate.of(2010, 1, 1)
+  val startDate = LocalDate.of(1991, 1, 1)
   val endDate = LocalDate.of(2020, 12, 31)
-
-  val daysCount = 1000
 
   import spark.implicits._
 
@@ -44,51 +40,39 @@ object DWDTest extends App {
     orderBy('sid, 'month, 'day)
   //  h.show
 
-//  val l0 = h.filter('sid === 1420).select('tavg).map(f => f.getDouble(0))
-//    .collectAsList.asScala.map(_.doubleValue)(breakOut)
   val l0 = h.filter('sid === 1420).select('tavg).map(f => f.getDouble(0))
     .collectAsList.asScala.map(_.doubleValue).toList
+  //  val l0 = h.filter('sid === 1420).select('tavg).map(f => f.getDouble(0))
+  //    .collectAsList.asScala.map(_.doubleValue)(breakOut)
   val l1 = h.filter('sid === 7341).select('tavg).map(f => f.getDouble(0))
     .collectAsList.asScala.map(_.doubleValue).toList
   val l2 = h.filter('sid === 917).select('tavg).map(f => f.getDouble(0))
     .collectAsList.asScala.map(_.doubleValue).toList
-  println(l0)
-  println(l0.getClass.getName)
 
-//  val x = List(12.0, -51.0, 4.0).toArray
-//  println(x)
-//  println("> "+x.getClass.getName)
-  val x = l0.toArray
-//  val y = List(6.0, 167.0, -68.0).toArray
-  val y = l1.toArray
-//  val z = List(-4.0, 24.0, -41.0).toArray
-  //val x = Seq(12.0, -51.0, 4.0)
-  //  val x = Array(12.0, -51.0, 4.0)
-  val rows: RDD[Vector] = spark.sparkContext.parallelize(Array(
-    Vectors.dense(x),
-    Vectors.dense(y)
-//    Vectors.dense(z),
-//    Vectors.dense(12.0, -51.0, 4.0),
-//    Vectors.dense(6.0, 167.0, -68.0),
-//    Vectors.dense(-4.0, 24.0, -41.0))
-  ))// an RDD of local vectors
+  val data = Array(
+    Vectors.dense(l0.toArray),
+    Vectors.dense(l1.toArray),
+    Vectors.dense(l2.toArray)
+  )
+
+  val rows: RDD[Vector] = spark.sparkContext.parallelize(data) // an RDD of local vectors
+  val nosv = rows.count().toInt // unless matrix will be transposed
 
   // Create a RowMatrix from an RDD[Vector].
-  //  val mat: RowMatrix = new RowMatrix(rows)
-  val mat = new RowMatrix(rows)
+  val mat = new RowMatrix(rows, nosv, 366)
 
-  // Get its size.
-  println(s"rows = ${mat.numRows()}")
-  println(s"cols = ${mat.numCols()}")
-  //      val mat = new RowMatrix(rows, 366, 2)
+  //  // Get its size.
+  //  println(s"rows = ${mat.numRows()}")
+  //  println(s"cols = ${mat.numCols()}")
+
 
   //  val qrResult = mat.tallSkinnyQR(true)
   //  qrResult.R
   //  21/04/30 19:27:29 WARN LAPACK: Failed to load implementation from: com.github.fommil.netlib.NativeSystemLAPACK
   //  21/04/30 19:27:29 WARN LAPACK: Failed to load implementation from: com.github.fommil.netlib.NativeRefLAPACK
 
-  // Compute the top 5 singular values and corresponding singular vectors.
-  val svd: SingularValueDecomposition[RowMatrix, Matrix] = mat.computeSVD(2, computeU = true)
+  // Compute the top 'nosv' singular values and corresponding singular vectors.
+  val svd: SingularValueDecomposition[RowMatrix, Matrix] = mat.computeSVD(nosv, computeU = true)
   val U: RowMatrix = svd.U // The U factor is a RowMatrix.
   val s: linalg.Vector = svd.s // The singular values are stored in a local dense vector.
   val V: Matrix = svd.V // The V factor is a local dense matrix.
